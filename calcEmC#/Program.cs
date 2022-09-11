d4d5ddb02cf1e967b170444d81eb05a59dbe356b
DevVictor37 <109398771+DevVictor37@users.noreply.github.com>
2022-09-11 17:23:43 -0300

    Add files via upload

---
+++ calcEmC#/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1, num2;
+
+            string tm;
+
+            Console.WriteLine("Digite um valor");
+            tm = Console.ReadLine();
+            num1 = int.Parse(tm);
+
+            Console.WriteLine("Digite outro número");
+            Console.ReadLine();
+            num2 = int.Parse(tm);
+
+            double resultado;
+
+            resultado = num1 + num2;
+            Console.WriteLine("Soma: " + resultado);
+
+            resultado = num1 - num2;
+            Console.WriteLine("Subtração: " + resultado);
+
+            resultado = num1 * num2;
+            Console.WriteLine("Multiplicação: " + resultado);
+
+            resultado = num1 / num2;
+            Console.WriteLine("Divisão: " + resultado);
+        }
+    }
+}
